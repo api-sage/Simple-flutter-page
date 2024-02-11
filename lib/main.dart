7597be52c849b4259ca0198e7508663597ff39f5
Paul Afolabi <paulafolabi745@gmail.com>
2024-02-11 11:35:28 +0100

    Description component implementation

--- lib/main.dart
+++ lib/main.dart
@@ -205,3 +205,24 @@ class RatingComponent extends StatelessWidget {
     );
   }
 }
+class DescriptionComponent extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 50.0, top: 5.0),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text(
+            "Description",
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 16.0,
+              color: Colors.black,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
