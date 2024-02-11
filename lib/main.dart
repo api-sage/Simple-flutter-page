7b59f28a09eb963d319e934e41f736ae12e670f9
Paul Afolabi <paulafolabi745@gmail.com>
2024-02-11 11:37:23 +0100

    Size component implementation

--- lib/main.dart
+++ lib/main.dart
@@ -279,3 +279,25 @@ class DividerComponent extends StatelessWidget {
     );
   }
 }
+class SizeComponent extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 50.0, top: 10.0),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            "Size",
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 16.0,
+              color: Colors.black,
+              fontFamily: "Sora",
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
